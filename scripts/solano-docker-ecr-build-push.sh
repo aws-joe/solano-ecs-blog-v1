76ad345ba21ed39cb85d85760bcfeaca972b0d63
root <root@ip-172-31-43-210.ec2.internal>
2016-03-16 22:37:39 +0000

    adding debug

--- scripts/solano-docker-ecr-build-push.sh
+++ scripts/solano-docker-ecr-build-push.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Build and start docker container
 
+SOLANO_LOGFILE="$HOME/results/$TDDIUM_SESSION_ID/session/solano-docker-ecr-build-push.sh.txt"
+echo "Starting solano-docker-ecr-build-push.sh" > $SOLANO_LOGFILE
+date >> $SOLANO_LOGFILE
+
 set -o errexit -o pipefail # Exit on error
 
 # Ensure aws-cli is installed and configured
@@ -28,8 +32,8 @@ fi
 #Log in to AWS ECR Docker Repository
 DOCKER_LOGIN_CMD="$AWS ecr get-login --region $AWS_DEFAULT_REGION"
 
-echo $DOCKER_LOGIN_CMD
-echo "Assigning repository credentials..."
+echo $DOCKER_LOGIN_CMD >> $SOLANO_LOGFILE
+echo "Assigning repository credentials..." >> $SOLANO_LOGFILE
 
 DOCKER_LOGIN=`sudo $DOCKER_LOGIN_CMD`
 $DOCKER_LOGIN
@@ -40,15 +44,15 @@ if [ $? -ne 0 ]; then
 fi
 
 # Build docker image
-echo "Performing docker build."
-sudo docker build -t $DOCKER_APP:$TDDIUM_SESSION_ID .
-echo "Completed docker build."
+echo "Performing docker build." >> $SOLANO_LOGFILE
+sudo docker build -t $DOCKER_APP:$TDDIUM_SESSION_ID . >> $SOLANO_LOGFILE
+echo "Completed docker build." >> $SOLANO_LOGFILE
 
 #tag image and push to AWS ECR
-sudo docker tag ${DOCKER_APP}:${TDDIUM_SESSION_ID} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID}
-echo "Pushing docker image to repository."
-sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID}
-echo "Image in repository."
+sudo docker tag ${DOCKER_APP}:${TDDIUM_SESSION_ID} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID} >> $SOLANO_LOGFILE
+echo "Pushing docker image to repository." >> $SOLANO_LOGFILE
+sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID} >> $SOLANO_LOGFILE
+echo "Image in repository." >> $SOLANO_LOGFILE
 
 # Start docker container and record ID and IP address
 CID=$(sudo docker run -d --expose=80 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID})
