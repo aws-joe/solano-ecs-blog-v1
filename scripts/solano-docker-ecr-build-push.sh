9a9b762da55b2ef46f7b867e67041a2fecca40f3
root <root@ip-172-31-43-210.ec2.internal>
2016-03-17 00:26:02 +0000

    still broke

--- scripts/solano-docker-ecr-build-push.sh
+++ scripts/solano-docker-ecr-build-push.sh
@@ -50,7 +50,7 @@ sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/$
 echo "Image in repository." >> $SOLANO_LOGFILE
 
 # Start docker container and record ID and IP address
-CID=$(sudo docker run -d --expose=80 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID})
-echo $CID > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.cid
-IP_ADDR=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CID)
-echo $IP_ADDR > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.ip
+#CID=$(sudo docker run -d --expose=80 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID})
+#echo $CID > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.cid
+#IP_ADDR=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CID)
+#echo $IP_ADDR > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.ip
