d6a5751aeb31531d67f86a20902e116f446f9ee3
aws-joe <fontesj@amazon.com>
2016-12-09 21:38:59 +0000

    updating for assume role

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 # Please see LICENSE.txt for applicable license terms and NOTICE.txt for applicable notices. 
 */
 
-$AppName = "Demo Web App v99";
+$AppName = "Demo Web App v98";
 
 ob_start();
 
